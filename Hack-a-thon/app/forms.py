d736f2e1e4eeb67738ffc6e14e4d59808a0a03f3
alvinsgithub <alvin.y@berkeley.edu>
2014-07-02 15:46:08 -0700

    xxx

--- Hack-a-thon/app/forms.py
+++ Hack-a-thon/app/forms.py
@@ -1,27 +1,32 @@
 '''
 Created on Jul 2, 2014
 
-@author: lan_xu
+@author: lan_xu, alvin_yau
 '''
 from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField, DateTimeField, DateField, IntegerField
-from wtforms.validators import Required, ValidationError
+from wtforms import TextField, PasswordField, DateTimeField, DateField, IntegerField, validators
+from wtforms.validators import Required, Email
+from datetime import date
+from wtforms.validators import ValidationError
+from models import User
 
 
 class LoginForm(Form):
-    username = TextField('username', validators = [Required('Username is required')])
+    username = TextField('username', validators = [Required('Email is required'), validators.Email()])
     password = PasswordField('password', validators = [Required('Password is required')])
 
 
 class RegisterForm(Form):
-    def unique_user_name(form, field):
-        return
+    def unique_email(form, field):
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is not None:
+            raise ValidationError('User already exists!')
     
     def retype_password_validate(form, field):
         if form.password.data != form.password2.data: 
             raise ValidationError('Password Must Match!')
     
-    username = TextField('username', validators = [Required('Username is required'), unique_user_name])
+    username = TextField('username', [Required('Email is required'), validators.Email(), unique_email])
     password = PasswordField('password', validators = [Required('Password is required')])
     password2 = PasswordField('password2', validators = [Required('Please Retype your password'), retype_password_validate])
 
