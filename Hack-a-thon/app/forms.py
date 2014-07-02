15d450d2da1f647af740809efd2bd33d921ccb5b
alvinsgithub <alvin.y@berkeley.edu>
2014-07-02 15:12:55 -0700

    Register Basic Prototype
    Just a basic prototype

--- Hack-a-thon/app/forms.py
+++ Hack-a-thon/app/forms.py
@@ -7,9 +7,23 @@ from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField, DateTimeField, DateField, IntegerField
 from wtforms.validators import Required
 from datetime import date
+from wtforms.validators import ValidationError
 
 class LoginForm(Form):
     username = TextField('username', validators = [Required('Username is required')])
     password = PasswordField('password', validators = [Required('Password is required')])
 
 
+class RegisterForm(Form):
+    def unique_user_name(form, field):
+        return
+    
+    def retype_password_validate(form, field):
+        if form.password.data != form.password2.data: 
+            raise ValidationError('Password Must Match!')
+    
+    username = TextField('username', validators = [Required('Username is required'), unique_user_name])
+    password = PasswordField('password', validators = [Required('Password is required')])
+    password2 = PasswordField('password2', validators = [Required('Please Retype your password'), retype_password_validate])
+    
+    
--- Hack-a-thon/app/views.py
+++ Hack-a-thon/app/views.py
@@ -5,7 +5,7 @@ Created on Jul 2, 2014
 '''
 from app import app, lm, sqldb
 from models import User
-from forms import LoginForm
+from forms import LoginForm, RegisterForm
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
 from datetime import datetime, date, time, timedelta
@@ -44,5 +44,24 @@ def logout():
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
-    form = LoginForm()
+    # TODO 
+    #===========================================================================
+    # if g.user is not None and g.user.is_authenticated():
+    #     return redirect(request.args.get('next') or url_for('home'))
+    #===========================================================================
+    def add_user(name, pwd):
+        user = User(username=name, password=pwd)
+        sqldb.session.add(user)
+        sqldb.session.commit()
+    form = RegisterForm()
+    if form.validate_on_submit():
+        add_user(form.username.data, form.password.data)
+        flash('OKAY')
+#         user = User.query.filter_by(username = form.username.data).first()
+#         if user is not None and user.check_password(form.password.data):
+#             login_user(user)
+#             return redirect(request.args.get('next') or url_for('home'))
+#         else:
+#             form.password.errors.append('Invalid Crednetials')
+#             flash('Invalid login. Please try again.')    
     return render_template("register.html", form = form)
